1974b3100823870163cda642d2db7ba1f89a6fed
paulosvdd05 <paulosvdd05@gmail.com>
2023-04-06 10:43:09 -0300

    ListItem botao direita editar e excluir

--- src/Views/UserList.js
+++ src/Views/UserList.js
@@ -1,10 +1,41 @@
 import React from 'react'
-import { Text, View, FlatList } from 'react-native'
+import { Text, View, FlatList, Alert } from 'react-native'
 import users from '../data/users'
-import { Avatar, ListItem } from 'react-native-elements'
+import { Avatar, Button, Icon, ListItem } from 'react-native-elements'
 
 
 export default props => {
+
+    function confirmUserDeletion(user){
+        Alert.alert('Excluir Usuário', 'Deseja excluir o usuário?',[
+            {
+                text: 'Sim',
+                onPress(){
+                    console.warn('delete' + user.id);
+                }
+            },
+            {
+                text: 'Não'
+            }
+        ])
+    }
+
+    function getActions(user){
+        return(
+            <>
+                <Button 
+                    onPress={() => props.navigation.navigate('UserForm', user)}
+                    type='clear'
+                    icon={<Icon name='edit' size={25} color='orange'/>}
+                />
+                <Button 
+                    onPress={() => confirmUserDeletion(user)}
+                    type='clear'
+                    icon={<Icon name='delete' size={25} color='red'/>}
+                />
+            </>
+        )
+    }
 
     function getUserItem({ item: user }) {
         return (
@@ -13,8 +44,11 @@ export default props => {
                 <ListItem.Content >
                     <ListItem.Title>{user.name}</ListItem.Title>
                     <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
-                   
                 </ListItem.Content>
+                <View style={{flexDirection:'row'}}>
+                    {getActions(user)}
+                </View>
+                
             </ListItem>
         )
     }
