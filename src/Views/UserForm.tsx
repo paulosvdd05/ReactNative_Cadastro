73939ae885dafb88362dc13f8808bf3ce2319c87
paulosvdd05 <paulosvdd05@gmail.com>
2023-04-06 10:32:08 -0300

    adicionando inputText para modificar nome(incompleto)

--- src/Views/UserForm.tsx
+++ src/Views/UserForm.tsx
@@ -1,10 +1,25 @@
-import React, {useState} from 'react'
-import {Text} from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, TextInput, StyleSheet } from 'react-native'
 
 
-export default ({route, navigation}) => {
-    const [user, setUser] = useState( route.params ?  route.params : {})
+export default ({ route, navigation }) => {
+    const [user, setUser] = useState(route.params ? route.params : {})
     return (
-        <Text>{user.id}</Text>
+        <View>
+            <Text>Name</Text>
+            <TextInput
+                onChangeText={name => setUser({ ...user, name })}
+                placeholder='Informe o Nome'
+                value={user.name}
+            />
+        </View>
     )
 }
+
+const style = StyleSheet.create({
+    input: {
+        height: 40,
+        
+
+    }
+})
