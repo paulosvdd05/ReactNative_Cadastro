cb19c09c792e499996cc95333bbba25fa030a671
paulosvdd05 <paulosvdd05@gmail.com>
2023-04-06 10:19:21 -0300

    criando arquivo users

---
+++ src/data/users.tsx
@@ -0,0 +1,30 @@
+export default [
+    {
+        id: 1,
+        name: 'João Silva',
+        email: 'joaosilva22@empmail.com',
+        avatarUrl:
+            'https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584_1280.png'
+    },
+    {
+        id: 2,
+        name: 'Carla Peres',
+        email: 'calitaperes@empemail.com',
+        avatarUrl:
+            'https://cdn.pixabay.com/photo/2014/04/03/10/32/user-310807_1280.png'
+    },
+    {
+        id: 3,
+        name: 'Roberto de Nobrega',
+        email: 'robnobrega@empmail.com',
+        avatarUrl:
+            'https://cdn.pixabay.com/photo/2017/01/31/21/23/avatar-2027366_1280.png'
+    },
+    {
+        id: 4,
+        name: 'Mateus Marques',
+        email: 'marquesmat@empmail.com',
+        avatarUrl:
+            'https://cdn.pixabay.com/photo/2016/04/01/11/25/avatar-1300331_1280.png'
+    },
+]
