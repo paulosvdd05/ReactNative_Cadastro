42e8f1832d7337054b2179b7f8967adb126302d4
paulosvdd05 <paulosvdd05@gmail.com>
2023-04-06 10:44:45 -0300

    adicionando elementos a tela UserForm

--- src/Views/UserForm.tsx
+++ src/Views/UserForm.tsx
@@ -1,25 +1,52 @@
 import React, { useState } from 'react'
 import { View, Text, TextInput, StyleSheet } from 'react-native'
+import { Button, Icon } from 'react-native-elements'
 
 
 export default ({ route, navigation }) => {
     const [user, setUser] = useState(route.params ? route.params : {})
     return (
-        <View>
+        <View style={style.form}>
             <Text>Name</Text>
-            <TextInput
+            <TextInput style={style.input}
                 onChangeText={name => setUser({ ...user, name })}
                 placeholder='Informe o Nome'
                 value={user.name}
             />
+            <Text>Email</Text>
+            <TextInput style={style.input}
+                onChangeText={email => setUser({ ...user, email })}
+                placeholder='Informe o Email'
+                value={user.email}
+            />
+            <Text>URL do Avatar</Text>
+            <TextInput style={style.input}
+                onChangeText={avatarUrl => setUser({ ...user, avatarUrl })}
+                placeholder='Informe a URL do Avatar'
+                value={user.avatarUrl}
+            />
+            <Button
+                onPress={ () => {
+                    navigation.goBack()
+                }}
+                icon={<Icon type='font-awesome' name='check' size={25} color='#fff' />}
+            />
         </View>
+
+
     )
 }
 
 const style = StyleSheet.create({
+    form: {
+        padding: 12
+
+    },
     input: {
         height: 40,
-        
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginBottom: 15,
 
     }
 })
