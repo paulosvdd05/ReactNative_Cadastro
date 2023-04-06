a4c2be47533e1c0be77944267b2e987d7462e7b3
paulosvdd05 <paulosvdd05@gmail.com>
2023-04-06 10:46:32 -0300

    arrumando vector-icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
 import React from 'react'
-import {NavigationContainer} from '@react-navigation/native'
-import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import UserList from './Views/UserList'
 import UserForm from './Views/UserForm'
+import { Button, Icon } from 'react-native-elements'
+
 
 const Stack = createNativeStackNavigator()
 
 export default props => {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName='UserList'>
-                <Stack.Screen 
+            <Stack.Navigator
+                initialRouteName='UserList'
+                screenOptions={screenOptions}
+            >
+                <Stack.Screen
                     name='UserList'
                     component={UserList}
+                    options={() => {
+                        return{
+                            title:'Lista de Usuários',
+                            headerRight: () =>(
+                                <Button 
+                                    type='clear'
+                                    icon={<Icon name='add' size={25} color='white' /> }
+                                />
+                            )
+                        }
+                    }}
                 />
-                <Stack.Screen 
+                <Stack.Screen
                     name='UserForm'
                     component={UserForm}
+                    options={{
+                        title:'Formulário de Usuários'
+                    }}
                 />
             </Stack.Navigator>
         </NavigationContainer>
     )
+}
+
+const screenOptions = {
+    headerStyle: {
+        backgroundColor: '#f4511e',
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+      },
 }
