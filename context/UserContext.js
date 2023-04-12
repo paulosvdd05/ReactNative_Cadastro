5be9830215cf21a851414a72908d9f3729c6062e
paulosvdd05 <paulosvdd05@gmail.com>
2023-04-12 08:09:27 -0300

    começando a implementar useReduce no projeto

--- context/UserContext.js
+++ context/UserContext.js
@@ -1,9 +1,17 @@
-import React, { createContext } from 'react'
+import React, { createContext, useReducer } from 'react'
 import users from '../src/data/users'
 
+const initialState = {users}
 const UsersContext = createContext({})
 
 export const UsersProvider = props => {
+
+    function reducer(state, action) {
+        console.warn(action);
+    }
+
+    const [state, dispatch] = useReducer(reducer, initialState)
+
     return (
         <UsersContext.Provider value={{
             state: {
